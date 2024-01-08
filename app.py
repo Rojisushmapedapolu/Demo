7a609a05fa109ff0972edc1e1f9746b0bf23789c
Rojisushmapedapolu <121299934+Rojisushmapedapolu@users.noreply.github.com>
2024-01-08 11:59:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import streamlit as st
-st.se_page_config(page_title='Cats')
-st.header("types of cats")
-col1,col2=st.columns(2)
+st.set_page_config(page_title='Cats)
+st.header("Types of Cats")
+
+col1, col2 = st.columns(2)
 with col1:
-  st.subheader("persian cat")
-  st.image("./persiancat.jpg",caption="persian cat",width=300,use_column_width=True)
-  st.write("persian cats are cute")
+  st.subheader("Persian Cat")
+  st.image("./PersianCat.jpg", caption="Persian Cat", width=300,use_column_width=True)
+  st.write("Persian cats are cute")
 with col2:
   st.subheader("Ragdoll Cat")
-  st.image("./ragdollcat.jpg",caption="Ragdoll Cat",eidth=300,use_column_width=True)
-  st.write("Ragdoll cats are proud")
+  st.image("./Ragdoll.jpg", caption="Ragdoll Cat", width=300,use_column_width=True)
+  st.write("Ragdoll cats are proud")
